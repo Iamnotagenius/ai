a4474613fed3f932e63b1dff359160b81985acac
fzzyhmstrs <fzzyhmstrs@gmail.com>
2024-08-27 18:22:46 -0400

    update 19.6 - config update

--- src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/config/ResourcesConfig.kt
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_imbuement/config/ResourcesConfig.kt
@@ -3,9 +3,11 @@ package me.fzzyhmstrs.amethyst_imbuement.config
 import me.fzzyhmstrs.amethyst_imbuement.AI
 import me.fzzyhmstrs.fzzy_config.annotations.ConvertFrom
 import me.fzzyhmstrs.fzzy_config.annotations.RequiresRestart
+import me.fzzyhmstrs.fzzy_config.annotations.Version
 import me.fzzyhmstrs.fzzy_config.config.Config
 import me.fzzyhmstrs.fzzy_config.validation.collection.ValidatedList
 
+@Version(1)
 @ConvertFrom("resources_v0.json", AI.MOD_ID)
 class ResourcesConfig: Config(AI.identity("resources_config")){
 
@@ -15,5 +17,13 @@ class ResourcesConfig: Config(AI.identity("resources_config")){
 
     @RequiresRestart
     var disabledResources = ValidatedList.ofString("optional/iridescent_scepter_imbuing", "optional/lustrous_scepter_imbuing")
+
+    override fun update(deserializedVersion: Int) {
+        if (deserializedVersion == 0) {
+            val list = disabledResources.get().toMutableList()
+            list.add("integration/citrine_crystals")
+            disabledResources.validateAndSet(list)
+        }
+    }
 
 }
